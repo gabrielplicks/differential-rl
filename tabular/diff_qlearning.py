3bb2dfb5d6310d3fa26f5f04c93e78d33dc511e0
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-10-24 18:23:19 -0300

    Minor changes

--- tabular/diff_qlearning.py
+++ tabular/diff_qlearning.py
@@ -6,7 +6,7 @@ import random
 from pprint import pprint
 
 
-class DiffQLearning():
+class TabularDiffQLearning():
     def __init__(self, env=None, alpha=0.01, beta=0.01, epsilon=0.1):
         # Environment
         if env == None:
@@ -25,35 +25,25 @@ class DiffQLearning():
         # Average-reward estimate
         self.R = 0
 
-        # Training
-        self.N_STEPS = 5000000
-
     
-    def epsilon_greedy(self, state):
-        if random.random() > self.epsilon:
-            return self.Q[state].index(max(self.Q[state]))
-        else:
-            return random.randrange(self.env.action_space.n)
+    def epsilon_greedy(self, state): 
+        if random.random() > self.epsilon: 
+            return np.argmax(self.Q[state])
+        else: 
+            return np.random.randint(0, self.env.action_space.n)
 
 
-    def train(self):
-        # Stats
-        # rewards = list()
-        # plt.ion()
-        # plt.show()
-        # plt.pause(0.0001)
-
+    def train(self, n_steps=1000000):
         # Reset environment
         state = self.env.reset()
-        state = tuple(state)
         # Add state to Q table if not there
         if state not in self.Q.keys(): self.Q[state] = np.zeros(self.env.action_space.n).tolist()
         # Start learning
-        for _ in range(self.N_STEPS):
-            # Select and apply action
+        for _ in range(n_steps):
+            # Choose action
             action = self.epsilon_greedy(state)
+            # Take action
             next_state, reward, _, _ = self.env.step(action)
-            next_state = tuple(next_state)
             # Add state to Q table if not there
             if next_state not in self.Q.keys(): self.Q[next_state] = np.zeros(self.env.action_space.n).tolist()
             # Learn
@@ -62,16 +52,10 @@ class DiffQLearning():
             self.Q[state][action] += self.alpha * delta
             # Transition
             state = next_state
-            
-            # Stats
-            # rewards.append(reward)
-            # plt.plot(rewards)
-            # plt.draw()
-            # plt.pause(0.0001)
 
 
 if __name__ == '__main__':
-    agent = DiffQLearning(env="AccessControl-v0")
-    agent.train()
+    agent = TabularDiffQLearning(env="AccessControl-v0")
+    agent.train(n_steps=20000000)
     pprint(agent.Q)
     pprint(agent.R)
