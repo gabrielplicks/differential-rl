97ae36008d41e24533ee74ee432c19484e3a01f7
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-11-06 20:43:48 -0300

    Training script

--- train.py
+++ train.py
@@ -1,21 +1,43 @@
 from tabular.diff_qlearning import TabularDiffQLearning
-from tabular.diff_sarsa import TabularDiffSarsa
+from approximation.replay_diff_qlearning import DiffQNetworkAgent
+from approximation.nonlinear_diff_qlearning import NonLinearDiffQNetworkAgent
 from pprint import pprint
 import gym
 import gym_accesscontrol
+import gym_inventory
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
 
+runs = 30
+n_steps = 200000
 
-# Make env
-accesscontrol = gym.make("AccessControl-v0")
+# Tabular Differential Q-Learning
+for run in range(1,runs+1):
+    print('Running {}...'.format(run))
+    accesscontrol = gym.make("AccessControl-v0", rand_seed=run)
+    agent = TabularDiffQLearning(env=accesscontrol, alpha=0.025, eta=0.5, epsilon=0.1, rand_seed=run)
+    Q, R, rewards, avg_rewards = agent.train(n_steps=n_steps)
+    np.save('results_tabulardiffqlearning/Q_{}.npy'.format(run), Q, allow_pickle=True)
+    np.save('results_tabulardiffqlearning/rewards_{}.npy'.format(run), rewards, allow_pickle=True)
+    np.save('results_tabulardiffqlearning/avg_rewards_{}.npy'.format(run), avg_rewards, allow_pickle=True)
 
-# Q-Learning
-qlearn = TabularDiffQLearning(env=accesscontrol)
-qlearn.train()
-pprint(qlearn.Q)
-pprint(qlearn.R)
+# Non-linear Differential Q-learning
+for run in range(1,runs+1):
+    print('Running {}...'.format(run))
+    accesscontrol = gym.make("AccessControl-v0", rand_seed=run)
+    agent = NonLinearDiffQNetworkAgent(env=accesscontrol, alpha=0.001, eta=0.05, epsilon=0.1, rand_seed=run)
+    qnet, R, rewards, avg_rewards, losses = agent.train(n_steps=n_steps)
+    np.save('results_nonlineardiffqlearning/losses_{}.npy'.format(run), losses, allow_pickle=True)
+    np.save('results_nonlineardiffqlearning/rewards_{}.npy'.format(run), rewards, allow_pickle=True)
+    np.save('results_nonlineardiffqlearning/avg_rewards_{}.npy'.format(run), avg_rewards, allow_pickle=True)
 
-# Sarsa
-sarsa = TabularDiffSarsa(env=accesscontrol)
-sarsa.train()
-pprint(sarsa.Q)
-pprint(sarsa.R)
+# Differential Q-network (experience replay)
+for run in range(1,runs+1):
+    print('Running {}...'.format(run))
+    accesscontrol = gym.make("AccessControl-v0", rand_seed=run)
+    agent = DiffQNetworkAgent(env=accesscontrol, alpha=0.0001, eta=0.05, epsilon=0.1, rand_seed=run)
+    qnet, qnet_target, R, rewards, avg_rewards, losses = agent.train(n_steps=n_steps)
+    np.save('results_diffqnetwork/losses_{}.npy'.format(run), losses, allow_pickle=True)
+    np.save('results_diffqnetwork/rewards_{}.npy'.format(run), rewards, allow_pickle=True)
+    np.save('results_diffqnetwork/avg_rewards_{}.npy'.format(run), avg_rewards, allow_pickle=True)
