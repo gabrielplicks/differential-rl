33d595b997438206e9a526274fc1af7a12938dc1
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-11-07 19:32:58 -0300

    Updated script to iterate over folders

--- plot.py
+++ plot.py
@@ -1,43 +1,51 @@
 # This plot script is based on @abhisheknaik96's script
 # In https://github.com/abhisheknaik96/average-reward-methods
 
+import numpy as np
+from matplotlib import pyplot as plt
+
+folders = ['results_diffqnetwork', 'results_nonlinearfreezediffqlearning', 'results_nonlineardiffqlearning', 'results_tabulardiffqlearning']
+labels = {'results_diffqnetwork':           'Diff. Q-network (xp-replay, freezing avg-reward)', 
+        'results_nonlinearfreezediffqlearning':   'Non-linear F.A. Diff. Q-learning (freezing avg-reward', 
+        'results_nonlineardiffqlearning':   'Non-linear F.A. Diff. Q-learning', 
+        'results_tabulardiffqlearning':     'Tabular Diff. Q-learning'}
+
+runs = 30
+n_steps = 200000
+
 # Load rewards
-tab_rewards = list()
-dqn_rewards = list()
-for run in range(1,runs+1):
-    tab_reward = np.load('results_diffqn/rewards_{}.npy'.format(run), allow_pickle=True)
-    dqn_reward = np.load('results/rewards_{}.npy'.format(run), allow_pickle=True)
-    tab_rewards.append(np.array(tab_reward))
-    dqn_rewards.append(np.array(dqn_reward))
-tab_rewards = np.array(tab_rewards)
-dqn_rewards = np.array(dqn_rewards)
+all_rewards = dict()
+for folder in folders:
+    all_rewards[folder] = list()
+    for run in range(1,runs+1):
+        rewards = np.load('{}/rewards_{}.npy'.format(folder, run), allow_pickle=True)
+        all_rewards[folder].append(np.array(rewards))
+    all_rewards[folder] = np.array(all_rewards[folder])
 
 # Get reward rates
-window = 2000
-sample = 800
+window = 1000
+sample = 1000
 conv_arr = np.ones(window)
-tab_reward_rates = list()
-dqn_reward_rates = list()
-for run in range(runs):
-    tab_reward_rate = np.convolve(tab_rewards[run], conv_arr, mode='valid') / window
-    dqn_reward_rate = np.convolve(dqn_rewards[run], conv_arr, mode='valid') / window
-    tab_reward_rates.append(tab_reward_rate)
-    dqn_reward_rates.append(dqn_reward_rate)
-tab_reward_rates = np.array(tab_reward_rates)
-dqn_reward_rates = np.array(dqn_reward_rates)
-tab_mean = np.mean(tab_reward_rates, axis=0)
-dqn_mean = np.mean(dqn_reward_rates, axis=0)
-tab_stderr = np.std(tab_reward_rates, axis=0) / np.sqrt(tab_reward_rates.shape[0])
-dqn_stderr = np.std(dqn_reward_rates, axis=0) / np.sqrt(dqn_reward_rates.shape[0])
-x_axis = np.arange(window, n_steps+1)[::sample]
-tab_y_axis = tab_mean[::sample]
-dqn_y_axis = dqn_mean[::sample]
-tab_stderr = tab_stderr[::sample]
-dqn_stderr = dqn_stderr[::sample]
-plt.plot(x_axis, tab_y_axis, label='Tabular Diff. Q-learning')
-plt.fill_between(x_axis, tab_y_axis + tab_stderr, tab_y_axis - tab_stderr, alpha=0.3)
-plt.plot(x_axis, dqn_y_axis, label='Diff. Q-network (xp-replay)')
-plt.fill_between(x_axis, dqn_y_axis + dqn_stderr, dqn_y_axis - dqn_stderr, alpha=0.3)
+for folder in folders:
+    all_reward_rates = list()
+    for run in range(runs):
+        reward_rates = np.convolve(all_rewards[folder][run], conv_arr, mode='valid') / window
+        all_reward_rates.append(reward_rates)
+    all_reward_rates = np.array(all_reward_rates)
+
+    # Compute mean and std error
+    mean = np.mean(all_reward_rates, axis=0)
+    stderr = np.std(all_reward_rates, axis=0) / np.sqrt(all_reward_rates.shape[0])
+
+    # Plot
+    x_axis = np.arange(window, n_steps+1)[::sample]
+    y_axis = mean[::sample]
+    stderr = stderr[::sample]
+    plt.plot(x_axis, y_axis, label=labels[folder])
+    plt.fill_between(x_axis, y_axis + stderr, y_axis - stderr, alpha=0.3)
+
+plt.ylabel('Reward rate')
+plt.xlabel('Step')
 plt.grid()
 plt.legend()
 plt.show()
