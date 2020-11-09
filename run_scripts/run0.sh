427aaf31d8d4ee36d2c5c071ee55a6e29b42d479
Gabriel Paludo Licks <gabrielplicks@gmail.com>
2020-11-09 19:43:51 -0300

    New scripts

--- run_scripts/run0.sh
+++ run_scripts/run0.sh
@@ -5,6 +5,4 @@ python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -
 python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 64 -b 32 -O diffqn 
 python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 64 -b 64 -O diffqn 
 python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 64 -b 128 -O diffqn 
-python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 64 -b 256 -O diffqn 
-python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 128 -b 32 -O diffqn 
-python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 128 -b 64 -O diffqn
+python3 train.py -A DiffQNetworkAgent -E AccessControl -r 15 -s 100000 -a 0.01 -e 0.5 -u 64 -b 256 -O diffqn
